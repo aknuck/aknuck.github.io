653ac849c867ebdf6a39bfe24bcbe658a4ce56ea
aknuck <adamknuckey@gmail.com>
2017-05-12 00:59:54 -0400

    tests

--- js/script.js
+++ js/script.js
@@ -78,6 +78,13 @@
 $(document).ready(function(){
 	document.body.addEventListener('touchmove', function(e){ e.preventDefault(); });
 	console.log("ready");
-	$('#main').on('swipedown',function(){alert('test');$('#main').append("swipedown..");} );
-	$('#main').on('swipeup',function(){$('#main').append("swipeup..");} );
+	$(document).on('swipedown', '.content', function () {
+	    alert("swipedown..");
+	});
+	$(document).on('swipeup', '.content', function () {
+	    alert("swipeup..");
+	});
 });
+
+
+
