9e025325d7a33c3a1f9cbecbdf7d946819c5ff19
aknuck <adamknuckey@gmail.com>
2017-05-12 00:29:41 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+CSE 264 Final
---
+++ js/script.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+	$('html').on('swipedown',function(){alert("swipedown..");} );
+	$('html').on('swipeup',function(){alert("swipeup..");} );
+});
