6f8ccb0fdc15b0832eb699ce1bc92095091c2a42
aknuck <adamknuckey@gmail.com>
2017-05-12 00:33:45 -0400

    tests

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
-	$('html').on('swipedown',function(){alert("swipedown..");} );
-	$('html').on('swipeup',function(){alert("swipeup..");} );
+	$('html').on('swipedown',function(){$('body').append("swipedown..");} );
+	$('html').on('swipeup',function(){$('body').append("swipeup..");} );
 });
