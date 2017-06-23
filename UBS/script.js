31826877b3755aa59e551185f430bf2ea7124ced
aknuck <adamknuckey@gmail.com>
2017-06-23 11:58:54 -0400

    fixed map move

--- UBS/script.js
+++ UBS/script.js
@@ -169,4 +169,25 @@ $(document).ready(function(){
         calculateDistances();
         $("#search-bar").easyAutocomplete(generateData());
     });
+
+    $('.sb-search-input').keyup(function (event){
+		if ($('.results').css('display') == 'none'){
+			if (event.which == 13 || event.keyCode == 13) {
+
+			}
+			else {
+				$(".results").slideDown();
+			}
+		}
+		else {
+			if (event.which == 13 || event.keyCode == 13){
+				$(".results").slideUp(); 
+			}
+			if (event.keyCode == 46 || event.keyCode == 8){
+				if ($.trim($('.sb-search-input').val()) == ''){
+					$(".results").slideUp();
+				}
+			}
+		}
+	});
 });
