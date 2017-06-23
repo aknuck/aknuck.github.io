4b7fc71809205b13589f03d4bb54f02eef3550b5
aknuck <adamknuckey@gmail.com>
2017-06-23 03:06:41 -0400

    fixing errors

--- UBS/script2.js
+++ UBS/script2.js
@@ -1,5 +1,6 @@
 
 var currentLocation; //lon and lat
+var myLocation; //map element
 
 function gotPos(position){
     console.log(position);
@@ -70,7 +71,7 @@ $(document).ready(function(){
         navigator.geolocation.getCurrentPosition(function(position) {
             lat = position.coords.latitude;
             lon = position.coords.longitude;
-            myLocation = marker([lat, lon]).addTo(map);
+            myLocation = L.marker([lat, lon]).addTo(map);
             //map.addLayer(myLocation);
             //myLocation.bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
             //.addTo(map);
